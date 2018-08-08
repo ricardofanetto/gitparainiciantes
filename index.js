0695023bf3ce0d35e71512a120812ea52557cfb8
Ricardo Netto <ricardofanetto@gmail.com>
2018-08-07 21:27:03 -0400

    Nova rota adicionada

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send({ message: 'Olá mundo esse é meu novo repositorio clonado.' })
-})
+});
+
+app.get('/ola', (req, res) => {
+    res.status(200).send({ message: 'Olá' });
+});
 
 app.listen(4001, () => {
     console.log('Api inicializada na porta 4001');
